98ee2455b16ccbcdb931a56319f5aced48b19012
R-Akila-2006 <akilaravikumar05@gmail.com>
2026-02-24 23:48:13 +0530

    Add basic Java program to print Hello Potter message

---
+++ Main.java
@@ -0,0 +1,9 @@
+// Main class - program execution starts from here
+public  class Main{
+    // main() is the entry point of every Java program
+    public static void main(String args[]){
+        
+        // Prints text on the screen
+        System.out.println("Hello Potter.....");
+    }
+}
